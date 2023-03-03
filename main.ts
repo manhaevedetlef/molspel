64a3ef737f59745e17ba28284a2c5c57720fd5c0
manhaevedetlef <58568927+manhaevedetlef@users.noreply.github.com>
2023-03-03 13:08:50 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -68,7 +68,27 @@ while(opKnopGedrukt==false){
         opKnopGedrukt = true
         game.isGameOver()
     }  //einde 3de if else
+
+    else if (posititieMol == 2 && input.buttonIsPressed(Button.AB)) {
+        opKnopGedrukt = true
+        basic.showIcon(IconNames.Yes)
+        game.addScore(1)
+    }//einde 2de if else
+    else if (posititieMol == 2 && input.buttonIsPressed(Button.AB)) {
+        opKnopGedrukt = true
+        game.isGameOver()
+    }  //einde 3de if else
+    else if (posititieMol == 2 && input.buttonIsPressed(Button.B)) {
+        opKnopGedrukt = true
+        game.isGameOver()
+    } //einde if else
+    else if (posititieMol == 2 && input.buttonIsPressed(Button.A)) {
+        opKnopGedrukt = true
+        game.isGameOver()
+    } //einde if else
     }  //while 2 tot hier
+
+
 
 } //while 1 einde
 
